c1372e348edbade06412452aa3041f2029a16392
lypinova <80448022+lypinova@users.noreply.github.com>
2023-04-05 14:34:52 +0000

    done

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,6 +4,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+       Person p = new Person ();
+       p.setFirstName("Kirill"); 
+       p.setLastName("Vuiko");
+       p.setAge(12); 
+       p.greetings(); 
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,36 @@
 public class Person {
-  
+  String firstName; 
+  String lastName; 
+  int age; 
+
+public void setFirstName (String firstName) { 
+          this.firstName = firstName; 
+} // end setter firstName
+
+public void setLastName (String lastName) { 
+this.lastName = lastName; 
+} // end setter lastName
+
+public void setAge (int age) { 
+  this.age = age; 
+} // end setter age; 
+
+public String getFirstName() { 
+return this.firstName;
+} // end getter firstName
+
+public String getLastName() {
+  return this.lastName;
 }
+
+public Integer getAge() { 
+  return this.age;
+}
+
+public void greetings() {
+  System.out.println("User's first name: " + getFirstName());
+  System.out.println("User's last name: " + getLastName());
+  System.out.println("User's age: " + getAge());
+}
+
+} // end class Person
